567ce1e8a04af93f12b225c2ed0b63bc7628312b
Mohamed <maaxix@gmail.com>
2018-11-25 22:22:12 +0100

    user interface + bootstrap

--- src/main/java/com/amaris/fab/controller/SCTOrderController.java
+++ src/main/java/com/amaris/fab/controller/SCTOrderController.java
@@ -1,10 +1,13 @@
 package com.amaris.fab.controller;
 
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 
 import com.amaris.fab.model.SCTOrder;
 import com.amaris.fab.model.SCTOrderDetails;
@@ -27,15 +33,15 @@ public class SCTOrderController {
 
 	@ResponseBody
 	@PostMapping(path = "/postSCT/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	SCTOrderDetails one(@PathVariable Long id) throws Exception {
-		SCTOrder order;
+	SCTOrderDetails one(@PathVariable Long id, @ModelAttribute @Valid SCTOrder order, BindingResult result) throws Exception {		
+		if(result.hasErrors())
+		{
+			//TODO:
+		}
 		
-		/* create dummy object for test */
-		order  = new SCTOrder();
+		SCTOrderDetails orderDetails = service.postSCTOrder(id,  order);
 		
-		SCTOrderDetails result = service.postSCTOrder(id,  order);
-		
-		return result;
+		return orderDetails;
 	}
 
 
